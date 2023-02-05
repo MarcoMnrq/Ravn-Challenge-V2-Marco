0460078e725cb8404b9b5a259e43f0f68a63c80b
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2023-02-04 19:17:18 -0500

    chore: configured prisma, swagger, cors & validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CORS configuration
+  app.enableCors({
+    origin: [],
+  });
+  // Global validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Tiny Store REST API')
+    .setDescription('RAVN Challenge v2')
+    .setVersion('1.0')
+    .setContact(
+      'Marco Manrique A.',
+      'https://github.com/MarcoMnrq',
+      'manriqueacham@gmail.com',
+    )
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+  // Versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(3000);
 }
 bootstrap();
